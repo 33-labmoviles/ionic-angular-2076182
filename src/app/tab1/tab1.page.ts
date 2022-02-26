39f0d5872b383b0da509d438fa7a9e080f3c5886
Riardo Rocha <ricardo.rocham@uanl.edu.mx>
2022-02-26 15:55:12 -0600

    Realiza lista de alumnos con foto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,57 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+    Alumnos=[
+      {Nombre: "Abraham ",
+      Apellido:"Ramirez",
+      Matricula:"123456"
+      },
+      {
+      Nombre:"Brian",
+      Apellido:"Esquivel",
+      Matricula:"123456"
+      },
+      {
+      Nombre:"Diego",
+      Apellido:"Davila",
+      Matricula:"2348932"
+      },
+      {
+      Nombre:"Diego",
+      Apellido:"Jasso",
+      Matricula:""
+      },
+      {
+      Nombre:"Julio",
+      Apellido:"Manuel",
+      Matricula:"1092744"
+      },
+      {
+      Nombre:"Luis",
+      Apellido:"Armando villanuevas",
+      Matricula:"984892342"
+      },
+      {
+      Nombre: "Luis",
+      Apellido:"Otiniel Tamez",
+      Matricula:"328498324"
+      },
+      {
+      Nombre: "Rogel ",
+      Apellido:"Axel",
+      Matricula:"12342134"
+      },
+      {
+      Nombre:"Luis ",
+      Apellido:"Armando",
+      Matricula:"1234213"
+      },
+      {
+      Nombre:"Ricardo ",
+      Apellido:"Rocha",
+      Matricula:"21341234"
+      }];
+
   constructor() {}
 
 }
