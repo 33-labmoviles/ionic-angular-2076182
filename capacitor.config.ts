b2156da19a8ac82c2b8ac217192fad43c43ee888
Riardo Rocha <ricardo.rocham@uanl.edu.mx>
2022-05-28 15:48:04 -0500

    PIAAAAAAAAAAAAAAAAAAAA

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-angular-2076182',
+  appName: 'app',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyACELWSYk7x3Oh8kAa5lfrh9SEBf_6RzSc",
   authDomain: "lab-aplicaciones.firebaseapp.com",
-  databaseURL: "https://lab-aplicaciones-default-rtdb.firebaseio.com",
   projectId: "lab-aplicaciones",
   storageBucket: "lab-aplicaciones.appspot.com",
   messagingSenderId: "519809668882",
@@ -11,11 +10,10 @@ export const environment = {
   production: true,
   firebaseConfig: {
     apiKey: "AIzaSyACELWSYk7x3Oh8kAa5lfrh9SEBf_6RzSc",
-  authDomain: "lab-aplicaciones.firebaseapp.com",
-  databaseURL: "https://lab-aplicaciones-default-rtdb.firebaseio.com",
-  projectId: "lab-aplicaciones",
-  storageBucket: "lab-aplicaciones.appspot.com",
-  messagingSenderId: "519809668882",
-  appId: "1:519809668882:web:80d479877a62c1621085cb"
+    authDomain: "lab-aplicaciones.firebaseapp.com",
+    projectId: "lab-aplicaciones",
+    storageBucket: "lab-aplicaciones.appspot.com",
+    messagingSenderId: "519809668882",
+    appId: "1:519809668882:web:80d479877a62c1621085cb"
   }
 };
