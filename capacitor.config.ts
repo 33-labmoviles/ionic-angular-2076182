efb55dc5fceabb01870cb02dd0612b0da5ed7e5a
Riardo Rocha <ricardo.rocham@uanl.edu.mx>
2022-05-28 10:52:36 -0500

    I,M,E de bd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-angular-1973592',
+  appName: 'ionic-angular-2076182',
   webDir: 'www',
   bundledWebRuntime: false
 };
